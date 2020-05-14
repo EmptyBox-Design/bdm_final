92058ca564d62d420e8c5f5a0737ebb0d9ab5071
BRANDON-RIG\Brandon Pachuca <bpachuca@kpf.com>
2020-05-14 09:24:30 -0400

    Create write_data.py

---
+++ write_data.py
@@ -0,0 +1,39 @@
+if __name__ == "__main__":
+
+  import csv
+  import sys
+
+  filename = sys.argv[1]
+
+  rows = []
+
+  # reading csv file 
+  with open(filename, 'r') as csvfile:
+
+    # creating a csv reader object 
+    csvreader = csv.reader(csvfile) 
+
+    # extracting field names through first row 
+    fields = next(csvreader) 
+
+    # extracting each data row one by one 
+    i = 0
+    for row in csvreader:
+      i+=1
+
+      rows.append(row)
+
+      if(i == 100000):
+        # name of csv file  
+        filename = "edit_cscl.csv"
+            
+        # writing to csv file  
+        with open(filename, 'w', newline='') as csvfile:  
+            # creating a csv writer object  
+            csvwriter = csv.writer(csvfile)  
+                
+            # writing the fields  
+            csvwriter.writerow(fields)  
+                
+            # writing the data rows  
+            csvwriter.writerows(rows)

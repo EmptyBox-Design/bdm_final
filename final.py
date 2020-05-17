5d6f9dea1e0ebcb2f8bb530b65343def3ed28c38
BRANDON-RIG\Brandon Pachuca <bpachuca@kpf.com>
2020-05-17 14:09:00 -0400

    Update final.py

--- final.py
+++ final.py
@@ -367,12 +367,10 @@ if __name__ == "__main__":
         .reduceByKey(lambda x,y: x+y) \
         .map(lambda x: (x[0].split("-")[0], (x[0].split("-")[1], x[1]))) \
         .groupByKey() \
-        .mapValues(lambda x: (x[0], sorted(x[1], key=lambda z: z[0], reverse=False))) \
+        .map(lambda x: (x[0], sorted(x[1], key=lambda z: z[0], reverse=False))) \
         .mapValues(lambda x: unpackTupes(x)) \
-        .sortByKey() \
         .map(toCSVLine) \
         .saveAsTextFile(output_location)
-    
-    
+
 
     print ("done processing!", ((time.time() - start_time) / 60), " minutes to run")
